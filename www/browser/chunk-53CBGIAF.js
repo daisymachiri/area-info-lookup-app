cd88d7290b556d7942a2772820c5c38cb5851caa
constancemachiri <machiriconstance@gmail.com>
2025-04-16 23:45:28 +0200

    Clean up old browser build files and update packages

--- www/browser/chunk-53CBGIAF.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c,d,e}from"./chunk-Y5XL4IAA.js";import"./chunk-MCRJI3T3.js";import"./chunk-3SN3MQ6X.js";import"./chunk-DWGPMRVG.js";import"./chunk-RW4GY4BD.js";export{e as MENU_BACK_BUTTON_PRIORITY,d as OVERLAY_BACK_BUTTON_PRIORITY,b as blockHardwareBackButton,a as shouldUseCloseWatcher,c as startHardwareBackButton};
